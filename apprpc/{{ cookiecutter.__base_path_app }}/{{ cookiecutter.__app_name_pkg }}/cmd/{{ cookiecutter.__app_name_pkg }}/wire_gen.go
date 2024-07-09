59fb1660721a8bb9a1e17a12e0ddd25f14b3da06
zhaojianhui <jianhui.zhao@sfygroup.com>
2024-07-09 11:50:28 +0800

    更新模板

--- apprpc/{{ cookiecutter.__base_path_app }}/{{ cookiecutter.__app_name_pkg }}/cmd/{{ cookiecutter.__app_name_pkg }}/wire_gen.go
+++ apprpc/{{ cookiecutter.__base_path_app }}/{{ cookiecutter.__app_name_pkg }}/cmd/{{ cookiecutter.__app_name_pkg }}/wire_gen.go
@@ -9,11 +9,11 @@ package main
 import (
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
-	"{{ cookiecutter.__project_name_snake }}/app/user/internal/biz"
-	"{{ cookiecutter.__project_name_snake }}/app/user/internal/conf"
-	"{{ cookiecutter.__project_name_snake }}/app/user/internal/data"
-	"{{ cookiecutter.__project_name_snake }}/app/user/internal/server"
-	"{{ cookiecutter.__project_name_snake }}/app/user/internal/service"
+	"{{ cookiecutter.__app_name_camel }}/app/{{ cookiecutter.__app_name_pkg }}/internal/biz"
+	"{{ cookiecutter.__app_name_camel }}/app/{{ cookiecutter.__app_name_pkg }}/internal/conf"
+	"{{ cookiecutter.__app_name_camel }}/app/{{ cookiecutter.__app_name_pkg }}/internal/data"
+	"{{ cookiecutter.__app_name_camel }}/app/{{ cookiecutter.__app_name_pkg }}/internal/server"
+	"{{ cookiecutter.__app_name_camel }}/app/{{ cookiecutter.__app_name_pkg }}/internal/service"
 )
 
 import (
